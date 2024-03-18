2f995c02ae72b5273732bf83a66378794e64eddc
dking072 <danielking4812@gmail.com>
2024-03-18 15:17:35 -0500

    dmrg support

--- las/hcircle.py
+++ las/hcircle.py
@@ -4,6 +4,7 @@ from pyscf import gto, scf, lib, mcscf
 import time
 import pandas as pd
 import math
+import os
 import seaborn as sns
 import matplotlib.pyplot as plt
 
@@ -135,6 +136,66 @@ class HCircle:
         las = las.state_average(las_weights,las_charges,las_spins,las_smults)
         las.max_cycle_macro = 100 
         return las
+
+from pyscf import dmrgscf, lib
+dmrgscf.settings.BLOCKEXE = os.popen("which block2main").read().strip()
+dmrgscf.settings.BLOCKEXE_COMPRESS_NEVPT = os.popen("which block2main").read().strip()
+dmrgscf.settings.BLOCKSCRATCHDIR = os.path.join(lib.param.TMPDIR, str(os.getpid()))
+dmrgscf.settings.MPIPREFIX = "" 
+
+class HDMRG(HCircle):
+    def __init__(self,dist,num_h,mval,scrdir="./dmrg_scrdir"):
+        self.data_name = f"hcircle_dmrg{num_h}_d{int(dist*10)}_m{mval}"
+        super().__init__(dist,num_h,2,fn=f"{self.data_name}.log")
+        self.mval = mval
+        self.rundir = f"./{self.data_name}/"
+        self.scrdir = scrdir
+
+    def make_casci(self,charge):
+        mol = self.get_mol()
+        mol.charge = charge
+        mol.spin = mol.nelectron%2
+        mol.build()
+
+        nelecas,ncas = mol.nelectron, mol.nao
+        twos = mol.spin
+        mc = mcscf.CASCI(mol,ncas,nelecas)
+        mc.mo_coeff = self.make_las_init_guess().mo_coeff
+        return mc
+
+    def make_dmrg(self,charge):
+        mc = self.make_casci(charge)
+        mc.fcisolver = dmrgscf.DMRGCI(mc.mol,maxM=self.mval)
+        mc.fcisolver.threads = lib.num_threads()
+        mc.fcisolver.spin = mc.mol.spin # setting to default spin here
+        mc.fcisolver.memory = int(mc.mol.max_memory / 1000)
+        mc.fcisolver.runtimeDir = f"{self.rundir}charge{charge}/"
+        mc.fcisolver.scratchDirectory = f"{self.scrdir}/"
+        mc.fcisolver.block_extra_keyword = ["onepdm"]
+        mc.verbose = 4
+        mc.canonicalization = False
+        return mc
+
+    def get_energy(self,mc):
+        fn = mc.fcisolver.runtimeDir
+        fn = f"{fn}/dmrg.out"
+        with open(fn,"r") as f:
+            lines = f.readlines()
+        dws = []
+        for line in lines:
+            if "DMRG Energy =" in line:
+                e = line.strip().split("=")[-1]
+            if "DW =" in line:
+                dws += [line.strip().split("|")[-1].split("=")[-1]]
+        e = float(e)
+        dws = np.array(dws).astype(float)
+        return e, dws[-1]
+
+    def run_dmrg(self,charge):
+        mc = self.make_dmrg(charge)
+        mc.kernel()
+        e, dw = self.get_energy(mc)
+        return e, dw
 
 
     
